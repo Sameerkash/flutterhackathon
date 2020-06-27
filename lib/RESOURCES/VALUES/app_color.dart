e8256f34d72bfae4bd4ff5829e252b118e33f6d2
Nazym <nazym.berdalyeva@gmail.com>
2020-06-27 23:21:24 +0600

    All colors in hexadecimal format

---
+++ lib/RESOURCES/VALUES/app_color.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Black = Color(0xFF000000);
+  static const Pink = Color(0xFFFB0060);
+  static const TiffanyBlue = Color(0xFF80CBC4);
+  static const DarkBlue = Color(0xFF2B2BD3);
+  static const LightBlack = Color(0xFF212121);
+  static const White = Color(0xFFFFFFFF);
+  static const WhiteShadow1 = Color(0xFFFFFFE6);
+  static const WhiteShadow2 = Color(0xFFFFFFD4);
+}
