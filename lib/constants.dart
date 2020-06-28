64c9855d2b532cec8a120630391fcfd0eb0529d9
YashMhatre <yashmhatre62@gmail.com>
2020-06-28 19:36:14 +0530

    profile screen done

---
+++ lib/constants.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFF090403);
+const Color kCardsColor = Color(0x021A2A);
+const Color kSecondaryColor = Color(0xFFEE912EFB);
+const Color kTertiaryColor = Colors.white;
+const Color kSecondCardColor = Color(0xFF191c21);
+
+List<String> crimes = <String>[
+  'Crime : Weaponized Drones',
+  'Crime : Secret Transactions',
+  'Crime : Destructive Deja Vu',
+];
+
+final List<String> categories = <String>[
+  'Category : Illegal Possesion',
+  'Category : Treachery',
+  'Category : Time Crimes'
+];
+
+List<String> severity = <String>[
+  'Severity : High',
+  'Severity : High',
+  'Severity : Severe',
+];
+
+List<String> date = <String>[
+  'Date : 19th June 2077',
+  'Date : 5th March 2070',
+  'Date : Unknown'
+];
